b5afa6de651b5142e45a9db316a75631fd16a3a4
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-05-06 14:20:19 +0800

    setup Copy over a simpler Zoom Delight version and add to the storybook

--- src/components/ZoomDelight/ZoomDelight.jsx
+++ src/components/ZoomDelight/ZoomDelight.jsx
@@ -3,93 +3,32 @@ import TweenMax from "gsap/TweenMax";
 import { Power1 } from "gsap/EasePack";
 import "./styles/zoom-delight.scss";
 
-/**
- * Zoom Delight wrapper enabling ZoomDelightClickable components with layout 
- * scaling capabilities.
- */
 class ZoomDelight extends Component {
 
     constructor(props) {
         super(props);
         this.state = {}
 
-        // Declate instance variables
-
-        /**
-         * @type {number}
-         */
         this.viewportWidth = window.innerWidth;
-
-        /**
-         * @type {number}
-         */
         this.viewportHeight = window.innerHeight;
-
-        /**
-         * @type {number}
-         */
         this.centerViewport = { x: this.viewportWidth / 2, y: this.viewportHeight / 2 };
-
-        /**
-         * @type {object}
-         */
         this.containerRef = null;
-
-        /**
-         * @type {object}
-         */
         this.moveableRef = null;
-
-        /**
-         * @type {number}
-         */
         this.origWidth = null;
-
-        /**
-         * @type {number}
-         */
         this.origHeight = null;
-        
-        /**
-         * @type {object}
-         */
         this.tween = null;
+        this.blur = 0;
 
-        /**
-         * @type {array}
-         */
-        this.childrenEl = [];
-
-        /**
-         * @type {object}
-         */
         this.animateData = {
             scale: 1,
             x: 0,
             y: 0
         };
 
-        /**
-         * @type {object}
-         */
-        this.animateOriginData = {
-            x: 0,
-            y: 0
-        }
-
-        /**
-         * @type {boolean}
-         */
         this.enabled = false;
 
-        /**
-         * @type {number}
-         */
         this.scale = 5;
 
-        /**
-         * @type {object}
-         */
         this.boxStyle = {
             "background": "black",
             "width": "200px",
@@ -98,158 +37,75 @@ class ZoomDelight extends Component {
             "display": "inline-block"
         };
 
-        // Pass instance context to methods
         this.onMouseDown = this.onMouseDown.bind(this);
         this.onScroll = this.onScroll.bind(this);
         this.onUpdateCallback = this.onUpdateCallback.bind(this);
-        this.onOriginUpdateCallback = this.onOriginUpdateCallback.bind(this);
-        this.onZoomDelightClickableClicked = this.onZoomDelightClickableClicked.bind(this);
     }
 
     componentDidMount() {
-        // Store the dimensions of the parent with all its children elements.
         this.origWidth = this.containerRef.clientWidth;
         this.origHeight = this.containerRef.clientHeight;
 
-        // Instantiate the scroll event.
+        this.containerRef.addEventListener("mousedown", this.onMouseDown);
         window.addEventListener("scroll", this.onScroll);
-
-        // Set initial transform origin point to the center point of the current viewport position.
         this.moveableRef.style.transformOrigin = this.centerViewport.x  + "px " + this.centerViewport.y  + "px";
     }
 
-    /**
-     * Fires as tween values change, updating the css transform values.
-     */
     onUpdateCallback() {
+        var progress = this.tween.progress();
+        var blur = 0;
+        if(progress < 0.5) {
+            blur = progress * 5;
+        } else {
+            blur = (1 - progress) * 5;
+        }
+        
+        //this.moveableRef.style.filter = "blur(" + blur + "px)";
         this.moveableRef.style.transform = "scale(" + this.animateData.scale + ") translate(" + this.animateData.x  + ", " + this.animateData.y  + ")";
     }
 
-    /**
-     * Updates the transform origin values of main element
-     */
-    onOriginUpdateCallback() {
-        this.moveableRef.style.transformOrigin = this.animateOriginData.x  + " " + this.animateOriginData.y;
-    }
-
-    /**
-     * Scroll event which determines whether or not to initiate zoomOut
-     */
     onScroll() {
         if(this.enabled) {
             this.zoomOut();
         }
     }
 
-    /**
-     * Initiates the zoom out animation, which brings it back to the original scale and position.
-     */
     zoomOut() {
         this.tween = TweenMax.to(this.animateData, .4, {scale: 1, x: "0px", y: "0px", ease: Power1.easeInOut, onUpdate: this.onUpdateCallback });
         this.enabled = false;
     }
 
-    /**
-     * Fires when there is a click.
-     * 
-     * @param {object} e Mouse click event object 
-     */
     onMouseDown(e) {
+    
         if(!this.enabled) {
-            this.zoomInToPoint({
-                x: e.clientX,
-                y: e.clientY + Math.abs(this.containerRef.getBoundingClientRect().top)
-            });
+            var x = e.clientX;
+            var y = e.clientY + Math.abs(this.containerRef.getBoundingClientRect().top);
+            this.centerYOrigin = this.centerViewport.y + document.documentElement.scrollTop;
+            var diff = (this.centerViewport.y - this.centerYOrigin) * (this.scale - 1) / this.scale;
+            var transformValues = { x: (this.centerViewport.x - x) / this.scale, y: (this.centerYOrigin - y) / this.scale};
+
+            this.centerYOrigin = this.centerViewport.y + document.documentElement.scrollTop;
+
+            this.moveableRef.style.transformOrigin = x  + "px " + y  + "px";
+            //this.moveableRef.style.transform = "scale(" + this.scale + ") translate(" + transformValues.x  + "px, " + transformValues.y  + "px)";
+
+            this.tween = TweenMax.to(this.animateData, .6, {scale: this.scale, x: transformValues.x  + "px", y: transformValues.y  + "px", 
+            ease: Power1.easeInOut, 
+            onUpdate: this.onUpdateCallback });
+            this.enabled = true;
         } else {
             this.zoomOut();
         }
-    }
-    
-    /**
-     * Determines the coordinates and scale values to zoom in to a point 
-     * while ensuring the target fits within the user's viewport.
-     * 
-     * @param {Object} p Data object with x and y coordinates
-     */
-    zoomInToPoint(p) {
-        var x = p.x;
-        var y = p.y;
-
-        // Get the y value of the main content container which is currently at the middle of the viewport.
-        this.centerYOrigin = this.centerViewport.y + document.documentElement.scrollTop;
-
-        // Get the new x and y positions based on the target coords and the center of the viewport, and divide by the value in which it scales
-        var transformValues = { x: (this.centerViewport.x - x) / this.scale, y: (this.centerYOrigin - y) / this.scale};
-
-        if(this.enabled) {
-            TweenMax.to(this.animateOriginData, .6, {x: x  + "px", y: y  + "px", 
-            ease: Power1.easeInOut, 
-            onUpdate: this.onOriginUpdateCallback });
-        } else {
-
-            this.animateOriginData.x = x;
-            this.animateOriginData.y = y;
-            this.moveableRef.style.transformOrigin = x  + "px " + y  + "px";
-        }
-
-        // Animate to the new target position and scale
-        this.tween = TweenMax.to(this.animateData, .6, {scale: this.scale, x: transformValues.x  + "px", y: transformValues.y  + "px", 
-        ease: Power1.easeInOut, 
-        onUpdate: this.onUpdateCallback });
-        this.enabled = true;
-
+        
     }
 
-    /**
-     * Callback that is passed to a zoomDelightClickable component, and when triggered, 
-     * fires the zoomInToPoint method.
-     * 
-     * @param {object} pos 
-     * @param {float} scale 
-     */
-    onZoomDelightClickableClicked(pos,scale) {
-        this.scale = scale;
-        this.zoomInToPoint(pos);
-    }
+    convertPixelPointToPercent(point) {
 
-    /**
-     * Recursive function which iterates through the hierarchy of children react components,
-     * and adds custom props to ZoomDelightClickable components.
-     * 
-     * @param {object} children 
-     */
-    filterChildren(children) {
-        let el = React.Children.map(children, (child) => {
-            var props;
-            
-            // Children with children must run the method on those objects.
-            if(child.props && child.props.children) {
-                let newChildren = this.filterChildren(child.props.children);
-                props = { ...child.props, children: newChildren};
-            } else {
-                props = { ...child.props };
-            }
-
-            // Checks to see if it is our needle, if so apply a prop with our callback.
-            if(child.type && child.type.name && child.type.name === "ZoomDelightClickable") {
-                props = { ...props, clickCallback: this.onZoomDelightClickableClicked};
-            }
-
-            // Ensure the child has a type set, if not it is some unknown React object
-            if(child.type) {
-                // return a copy of the new React component.
-                return React.cloneElement(child, props);
-            }
-            
-       });
-
-       return el;
     }
 
     render() {
         const { children } = this.props;
-        var childrenEl = this.filterChildren(children);
- 
+
         let styles = {
             "display": "none",
             "background": "blue",
@@ -263,14 +119,14 @@ class ZoomDelight extends Component {
             "margin-left": "-10px",
             "z-index": "999"
         };
-        
+
         return (
             <>
             <div style={styles}>
             </div>
             <div className="zoom-delight__container" ref={div => this.containerRef = div}>
                 <div className="zoom-delight__moveable" ref={div => this.moveableRef = div}>
-                    {childrenEl ? childrenEl : ""}
+                    { children }
                 </div>
             </div>
             </>
---
+++ src/components/ZoomDelight/ZoomDelight.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ZoomDelight from './ZoomDelight';
+
+export default {
+  title: 'ZoomDelight',
+};
+
+export const Default = () => {
+  return (
+    <ZoomDelight>
+      <div style={{width: '100%', height: '100vh'}}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum diam ac lorem tincidunt fringilla. In eget lacus ullamcorper, lacinia sapien et, vulputate augue. Nullam non magna et augue consequat egestas cursus nec erat. Donec dui augue, mattis in lectus nec, mattis ornare tortor. Maecenas eget auctor lorem. Suspendisse at leo fermentum, ultricies velit vel, interdum diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+Nulla blandit augue quis felis bibendum, ut molestie urna tempor. Pellentesque id placerat nisi. Vivamus ac elementum est. Morbi feugiat lorem ipsum, at varius leo suscipit sed. Mauris consectetur lacinia interdum. Mauris non vulputate arcu. Nunc et tristique leo. Quisque feugiat elementum lectus, vel sagittis nulla congue vel. Phasellus nec consectetur lorem. Vivamus id tristique tortor.
+
+Sed non nulla venenatis risus finibus fringilla vitae ut tortor. Nam sit amet tristique tortor. Duis congue luctus enim. Sed sit amet odio hendrerit tortor dapibus tempor. Ut eleifend facilisis tincidunt. Duis sed purus pellentesque, fringilla diam id, ullamcorper dui. Pellentesque laoreet hendrerit libero eget suscipit. Integer vitae consectetur massa. Phasellus vulputate pharetra turpis eget aliquet. Proin laoreet blandit quam quis tristique. Vivamus nulla est, fermentum et interdum et, egestas at lectus.
+
+Etiam eget semper massa. Nam facilisis lorem eu massa gravida, id rutrum neque consequat. Sed tristique quam turpis, non vehicula elit luctus id. Pellentesque dui libero, tincidunt nec libero eleifend, ullamcorper efficitur erat. Sed vestibulum massa justo. Nullam pretium vitae mauris nec fermentum. Curabitur aliquam elit vitae turpis pellentesque, at volutpat quam mollis. Nulla imperdiet massa et odio tempor sagittis. Nam leo nisl, hendrerit sit amet congue ac, tempor a felis.
+
+Mauris placerat odio vel neque consectetur congue. Suspendisse at arcu ipsum. Etiam in scelerisque ante. Phasellus aliquam, lectus ut egestas elementum, leo arcu tincidunt tortor, in cursus mi metus sit amet justo. Sed eu malesuada tellus. Vivamus iaculis scelerisque orci et fringilla. Sed sit amet neque ut urna porttitor aliquet. Donec tellus enim, rutrum vitae ullamcorper quis, hendrerit quis dolor. Aenean sed porta ligula.
+      </div>
+    </ZoomDelight>
+  )
+}
